321009b3b00f4320283a1da3c20206264a7dcc33
knightch54 <knightchrome54@gmail.com>
2022-05-19 21:54:31 +0300

    add attachments example

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -4,6 +4,7 @@ class OrderMailer < ApplicationMailer
   def create_order
     @user = params[:user]
     # @order = params[:order]
+    attachments['order_example.pdf'] = File.read("public/user_orders/order_example.pdf")
     mail(to: @user.email, subject: 'Thank you for your order')
   end
 end
